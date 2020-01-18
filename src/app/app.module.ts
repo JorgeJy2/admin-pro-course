3a97bd1b757ebdb5ced2fb2fcbd499357d0d3a54
jorgeJy2 <jorge-jy2@hotmail.com>
2020-01-17 18:12:50 -0600

    feat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 import { HeaderComponent } from './shared/header/header.component';
@@ -22,9 +19,9 @@ import { RegisterComponent } from './login/register/register.component';
     AppComponent,
     LoginComponent,
     NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
+    // DashboardComponent,
+    // ProgressComponent,
+    // Graficas1Component,
     HeaderComponent,
     SidebarComponent,
     BreadcrumbsComponent,
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+
+@NgModule({
+
+    declarations: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ]
+})
+
+export class PageModule {}
